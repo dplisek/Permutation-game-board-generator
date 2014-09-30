3bb0fdd670cfc3dc5493a03e44c8d58e10082251
Dominik Plisek <Dominik@e-77-41.eduroam.fit.cvut.cz>
2014-09-30 10:14:19 +0200

    Initial Commit

---
+++ PEK generator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PEK generator
+//
+//  Created by Dominik Plisek on 30/09/14.
+//  Copyright (c) 2014 Dominik Plisek a Tomas Marek. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
